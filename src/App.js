9d2e3920a3e22e39076e48b24e5c62c7b7ef9dd4
hu1won <ehql713@gmail.com>
2021-08-11 16:12:25 +0900

    fix second

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Calendar } from 'antd'; //npm install antd
-import './App.css';
-import Calender from './calender'
 
-const { Header, Content, Footer } = Layout;
-const names = ['Main','Service','Member','Gallery','Calender','Etc','Contact'];//카테고리
+import Layout from './components/Layout/Layout';
 
-const App = () => (
-  
-  <div className="App">
-   <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        {new Array(7).fill(null).map((_, index) => {
-          const key = index + 1;
-          return <Menu.Item key={key}>{`${names[key-1]}`}</Menu.Item>; //key 값을 이용해 전달받기
-        })}
-      </Menu>
-    </Header>
-    <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }} />
-      <div className="site-layout-content"><Calendar /></div>
-    </Content>
-        
-    <Footer style={{ textAlign: 'center' }}>COLLA-STUDY 2021</Footer>
-  </Layout>
-  </div>
-);
+function App() {
+  return <Layout />;
+}
 
 export default App;
